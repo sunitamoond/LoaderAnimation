8c727bc79981d84306dae1068d28e75f67969f95
Sunita Moond <sunitamnit@gmail.com>
2019-09-05 17:00:39 +0530

    circular loader

--- Example/LoaderAnimation/ViewController.swift
+++ Example/LoaderAnimation/ViewController.swift
@@ -7,17 +7,26 @@
 //
 
 import UIKit
+import LoaderAnimation
 
 class ViewController: UIViewController {
 
+    @IBOutlet var circularProgressBar: LoaderAnimation!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+//        let a = circularProgressBar
+//         self.circularProgressBar.addSubview(a)
+        DispatchQueue.main.asyncAfter(wallDeadline: .now() + 0.5) {
+            self.circularProgressBar.createSublayers();
+           self.circularProgressBar.createAnimation()
+           
+        }
     }
 
 }
